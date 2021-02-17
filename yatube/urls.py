0935396eb874d60d3bb9c3795366561ee9dfba20
Сергей <7atom7@mail.ru>
2021-02-17 15:32:34 +0300

    First final

--- yatube/urls.py
+++ yatube/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, 
+    urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, 
+    urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
